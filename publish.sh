d5993189b65c259d511229f19f73b476f08224ef
Goncalo Oliveira <goncalo.oliveira@redpanda.sh>
2020-11-24 12:46:41 +0100

    modified publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# linux x64
-echo "Publishing Linux x64..."
-dotnet publish -c release -o published -r linux-x64 src/faas-run.csproj
-zip -r -j published/faas-run-linux-x64.zip published/faas-run
-
 # osx x64
 echo "Publishing OSX x64..."
 dotnet publish -c release -o published -r osx-x64 src/faas-run.csproj
@@ -15,3 +10,7 @@ echo "Publishing Windows x64..."
 dotnet publish -c release -o published -r win10-x64 src/faas-run.csproj
 zip -r -j published/faas-run-win10-x64.zip published/faas-run.exe
 
+# linux x64
+echo "Publishing Linux x64..."
+dotnet publish -c release -o published -r linux-x64 src/faas-run.csproj
+zip -r -j published/faas-run-linux-x64.zip published/faas-run
