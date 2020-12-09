9b9e6706766aa2fcdb91a27a646cc1fa6dfdc45d
Goncalo Oliveira <goncalo.oliveira@redpanda.sh>
2020-12-09 16:53:28 +0100

    secret key from configuration changed

--- src/Configuration/SecretsConfigurationProvider.cs
+++ src/Configuration/SecretsConfigurationProvider.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Extensions.Configuration.OpenFaaSSecrets
 
                 foreach ( var secret in secrets )
                 {
-                    var secretName = string.Concat( "openfaas_secret_", Path.GetFileName( secret ) );
+                    var secretName = string.Concat( "_secret_", Path.GetFileName( secret ) );
                     var secretValue = File.ReadAllBytes( secret );
 
                     Data.Add( secretName, Convert.ToBase64String( secretValue ) );
